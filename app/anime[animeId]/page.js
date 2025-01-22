354e91909335134d14f93e3b2249e7d661788948
haider-9 <haiderahmad352@gmail.com>
2025-01-22 15:15:17 +0500

    fixed carousal

--- app/anime[animeId]/page.js
+++
@@ -1,6 +0,0 @@
-if (data.Media) {
-  setCoverImage(data.Media.bannerImage || "");
-  setPosterImage(data.Media.coverImage.extraLarge || data.Media.coverImage.large || "");
-  setAnimeImages(data.Media.images?.edges?.map(edge => edge.node) || []);
-  // ... rest of your existing state updates
-}
